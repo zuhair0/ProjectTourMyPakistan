4264652f68a9052888ccef1715df66de66e5b88d
dell <dell@DESKTOP-PO81H2P>
2022-11-25 07:10:29 -0800

    tourcompanyreg

--- ClassLibraryDAL/TourCompanyRegDAL.cs
+++ ClassLibraryDAL/TourCompanyRegDAL.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ClassLibraryModel
 namespace ClassLibraryDAL
 {
     public class TourCompanyRegDAL
     {
+        public static int SaveTourCompanyReg(TourCompanyRegModel tcrm)
+        {
+            SqlConnection con = DBHelper.GetConnection();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@", );
+            cmd.Parameters.AddWithValue("@", );
+            cmd.Parameters.AddWithValue("@", );
+            cmd.Parameters.AddWithValue("@", );
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            return i;
+        }
     }
 }
