84e23b658d9ae518084e60dbf6082675d30d078f
Sameer Siddiqui <Sameer.Siddiqui@studentambassadors.com>
2022-11-25 16:21:01 +0500

    Added my DB Connectionstring

--- ClassLibraryDAL/DBHelper.cs
+++ ClassLibraryDAL/DBHelper.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
-
+//Sameer's SQL (Don't Delete) = DESKTOP - ISNN91B\SQL2019
 namespace ClassLibraryDAL
 {
     public class DBHelper
     {
         public static SqlConnection GetConnection()
         {
-            SqlConnection con = new SqlConnection("Data Source=DESKTOP-PO81H2P\\DELL1;Initial Catalog=Tourism;Integrated Security=True");
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-ISNN91B\\SQL2019;Initial Catalog=Tourism;Integrated Security=True");
             return con;
         }
     }
