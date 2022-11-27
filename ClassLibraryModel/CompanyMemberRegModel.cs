6abd7f35a1696fa59158493ed7f7beb4f0b3ea85
dell <dell@DESKTOP-PO81H2P>
2022-11-27 03:50:58 -0800

    SPs

--- ClassLibraryModel/CompanyMemberRegModel.cs
+++ ClassLibraryModel/CompanyMemberRegModel.cs
@@ -24,3 +24,32 @@ namespace ClassLibraryModel
     }
 }
 //Don't know the Stored Procedures maazrat :')
+
+//use[Tourism]
+
+//create proc Sp_AddCompanyMember
+//@Tour_Member_ID int, @Company_Member_Firstname varchar(20), 
+//@Company_Member_Lastname varchar(20),@Company_Member_DOB varchar(20), @Company_Member_Gender varchar(20), 
+//@Company_Member_role varchar(20), @Company_Member_CNIC int, @Company_Member_Whatsapp int, @Company_Member_Mobile int,
+//@City_ID int, @Company_Member_Sector varchar(20), @Company_Member_registration_datetime date
+//as begin
+//insert into Company_Member_Registration([Tour_Member_ID], [Company_Member_Firstname],
+//[Company_Member_Lastname], [Company_Member_DOB], [Company_Member_Gender], [Company_Member_role],
+//[Company_Member_CNIC], [Company_Member_Whatsapp], [Company_Member_Mobile], [City_ID], [Company_Member_Sector], 
+//[Company_Member_registration_datetime], [Company_Member_registration_status])
+//values(@Tour_Member_ID, @Company_Member_Firstname,
+//@Company_Member_Lastname, @Company_Member_DOB, @Company_Member_Gender,
+//@Company_Member_role, @Company_Member_CNIC, @Company_Member_Whatsapp, @Company_Member_Mobile,
+//@City_ID, @Company_Member_Sector, @Company_Member_registration_datetime, 'true')
+//end
+
+//create proc Sp_GetCompanyMembers
+//as begin
+//select * from Company_Member_Registration
+//end
+
+//create proc Sp_DeleteCompanyMember
+//@Company_Member_ID int
+//as begin
+//delete from Company_Member_Registration where Company_Member_ID=@Company_Member_ID
+//end
