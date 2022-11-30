e8a819579759b90c2827fd039a5ebd49bb69d9f5
dell <dell@DESKTOP-PO81H2P>
2022-11-30 07:26:02 -0800

    Tour company service complete

---
+++ ClassLibraryDAL/TourPackageDAL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryModel;
+namespace ClassLibraryDAL
+{
+    public class TourPackageDAL
+    {
+        public static int SaveTourPackage(TourPackageModel tpm)
+        {
+            SqlConnection con = DBHelper.GetConnection();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Sp_SaveTourPackage", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Tour_Guide_ID",tpm.Tour_Guide_ID);
+            cmd.Parameters.AddWithValue("@PKG_Title",tpm.PKG_Title);
+            cmd.Parameters.AddWithValue("@PKG_Duration",tpm.PKG_Duration);
+            cmd.Parameters.AddWithValue("@PKG_Pricing",tpm.PKG_Pricing);
+            cmd.Parameters.AddWithValue("@PKG_From_City",tpm.PKG_From_City);
+            cmd.Parameters.AddWithValue("@PKG_To_City",tpm.PKG_To_City);
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            return i;
+        }
+    }
+}
---
+++ ClassLibraryModel/TourPackageModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryModel
+{
+    public class TourPackageModel
+    {
+        public int PKG_Id { get; set; }
+        public int Tour_Guide_ID { get; set; }
+        public string? PKG_Title { get; set; }
+        public string? PKG_Duration { get; set; }
+        public string? PKG_Pricing { get; set; }
+        public string? PKG_From_City { get; set; }
+        public string? PKG_To_City { get; set; }
+
+
+
+
+    }
+}
