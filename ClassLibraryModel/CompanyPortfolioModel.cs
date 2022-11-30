f81c278afe89eebfe13ad9bbd6260831cbe64169
ASH <ASH@DESKTOP-NTASNM1>
2022-11-30 11:28:41 +0500

    procedures

--- ClassLibraryModel/CompanyPortfolioModel.cs
+++ ClassLibraryModel/CompanyPortfolioModel.cs
@@ -15,3 +15,22 @@ namespace ClassLibraryModel
 
     }
 }
+
+//create proc Sp_SaveCompanyPortfolio
+//@Company_Id int, @Portfolio_details varchar(50)
+//as
+//begin
+//insert into Company_portfolio ([Company_Id], [Portfolio_details])
+//values(@Company_Id, @Portfolio_details)
+//end
+
+//create proc Sp_GetCompanyPortfolio
+//as begin
+//select * from Company_portfolio
+//end
+
+//create proc Sp_DeleteCompanyPortfolio
+//@Portfolio_id int
+//as begin
+//delete from Company_portfolio where Portfolio_id=@Portfolio_id
+//end
