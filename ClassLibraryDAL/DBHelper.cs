8d7612e0aa4c13014d6c81db26e798826b30470d
dell <dell@DESKTOP-PO81H2P>
2022-12-01 03:04:33 -0800

    .

--- ClassLibraryDAL/DBHelper.cs
+++ ClassLibraryDAL/DBHelper.cs
@@ -14,7 +14,7 @@ namespace ClassLibraryDAL
         public static SqlConnection GetConnection()
         {
            // AYESHA CONNECTION SqlConnection con = new SqlConnection("Data Source=DESKTOP-NTASNM1\\DELL;Initial Catalog=Tourism;Integrated Security=True");
-            SqlConnection con = new SqlConnection("Data Source=DESKTOP-NTASNM1\\DELL;Initial Catalog=Tourism;Integrated Security=True");
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-PO81H2P\\DELL1;Initial Catalog=Tourism;Integrated Security=True");
             return con;
         }
     }
