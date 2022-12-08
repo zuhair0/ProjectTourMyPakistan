939fb7850d31cc5f11704a3848767b0013565143
Sameer Siddiqui <Sameer.Siddiqui@studentambassadors.com>
2022-12-08 15:42:45 +0500

    Created Tour PKG Services DAL and Model

---
+++ ClassLibraryDAL/TourPackageServicesDAL.cs
@@ -0,0 +1,53 @@
+﻿using ClassLibraryModel;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryDAL
+{
+    public class TourPackageServicesDAL
+    {
+        public static int SaveTPServices(TourPackageServicesModel tpsm)
+        {
+            SqlConnection con = DBHelper.GetConnection();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Sp_SaveTourPkgServices", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Company_Service_ID", tpsm.Company_Service_ID);
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            return i;
+        }
+        public static List<TourPackageServicesModel> GetTourPackageServices()
+        {
+            SqlConnection con = DBHelper.GetConnection();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Sp_GetTourPkgServices", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            SqlDataReader sdr = cmd.ExecuteReader();
+            List<TourPackageServicesModel> TourPKGServices = new List<TourPackageServicesModel>();
+            while (sdr.Read())
+            {
+                TourPackageServicesModel tps = new TourPackageServicesModel();
+                tps.Company_Service_ID = int.Parse(sdr["Company_Service_ID"].ToString());
+                TourPKGServices.Add(tps);
+            }
+            con.Close();
+            return TourPKGServices;
+        }
+        public static int DeleteTourPkgServices(int Company_Service_ID)
+        {
+            SqlConnection con = DBHelper.GetConnection();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Sp_DeleteTourPkgService", con);
+            cmd.Parameters.AddWithValue("@Company_Service_ID", con);
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            return i;
+        }
+    }
+    
+}
---
+++ ClassLibraryModel/TourPackageServicesModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryModel
+{
+    public class TourPackageServicesModel
+    {
+        public int PKG_Service_ID { get; set; }
+        public int Company_Service_ID { get; set; }
+    }
+}
+//create proc Sp_SaveTourPkgServices
+//@Company_Service_ID int
+//as begin 
+//insert into Tour_Package_Services([Company_Service_ID])values(@Company_Service_ID)
+//end
+
+//create proc Sp_GetTourPkgServices
+//as begin select * from Tour_Package_Services
+//end
+
+//create proc Sp_DeleteTourPkgServices
+//@Company_Service_ID int
+//as begin delete from Tour_Package_Services where Company_Service_ID=@Company_Service_ID
+//end
