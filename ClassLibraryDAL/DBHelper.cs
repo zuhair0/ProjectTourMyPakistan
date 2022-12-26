4568f6fdd08425641d60bdd4d8e6ccae561dfc12
Sameer Siddiqui <Sameer.Siddiqui@studentambassadors.com>
2022-12-26 23:02:16 +0500

    Frontend of TourMyPakistan in process...

--- ClassLibraryDAL/DBHelper.cs
+++ ClassLibraryDAL/DBHelper.cs
@@ -4,20 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
-//Sameer's SQL (Don't Delete) = DESKTOP-ISNN91B\SQL2019
-//Zuhair's SQL DESKTOP-PO81H2P\DELL1
-// AYESHA CONNECTION SqlConnection con = new SqlConnection("Data Source=DESKTOP-NTASNM1\\DELL;Initial Catalog=Tourism;Integrated Security=True");
 namespace ClassLibraryDAL
 {
     public class DBHelper
     {
         public static SqlConnection GetConnection()
-        { //check box ki val kasay lyni ha
-            //cancatenate keasy krna comma seperated
-            //SqlConnection con = new SqlConnection("Data Source=DESKTOP-ISNN91B\\SQL2019;Initial Catalog=Tourism;Integrated Security=True");
-           // AYESHA CONNECTION SqlConnection con = new SqlConnection("Data Source=DESKTOP-NTASNM1\\DELL;Initial Catalog=Tourism;Integrated Security=True");
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-ISNN91B\\SQL2019;Initial Catalog=Tourism;Integrated Security=True");
+            //SqlConnection con = new SqlConnection("Data Source=DESKTOP-NTASNM1\\DELL;Initial Catalog=Tourism;Integrated Security=True");
             //SqlConnection con = new SqlConnection("Data Source=DESKTOP-PO81H2P\\DELL1s;Initial Catalog=Tourism;Integrated Security=True");
-            SqlConnection con = new SqlConnection("Data Source=DESKTOP-PO81H2P\\DELL1;Initial Catalog=Tourism;Integrated Security=True");
             return con;
         }
     }
